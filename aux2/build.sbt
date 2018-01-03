67850d1ed5e66af097ad0f51503ea704add60087
Oleksandr Rudak <Oleksandr_Rudak@epam.com>
2018-01-03 16:11:31 +0100

    Adding Scala project

---
+++ aux2/build.sbt
@@ -0,0 +1,5 @@
+name := "aux2"
+
+version := "0.1"
+
+scalaVersion := "2.10.7"
---
+++ aux2/src/main/scala/com/epam/TestScala.scala
@@ -0,0 +1,7 @@
+package com.epam
+
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
